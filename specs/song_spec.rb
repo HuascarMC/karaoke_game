1925e7781d26aa2dc5c5a14961db6546f67fcbb0
Huascar <huascarmc@hotmail.com>
2017-10-06 20:31:41 +0100

    call song name, call song author test passed

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -8,5 +8,12 @@ class TestSong < MiniTest::Test
     @song02 = Song.new("Dancing Queen", "ABBA")
   end
 
+  def test_song_name
+    assert_equal("Theme song", @song01.name)
+  end
+
+  def test_song_author
+    assert_equal("ABBA", @song02.author)
+  end
 
 end
